d1e35f294075f5824586516e5a14d3605b16464a
morgan_p <morgan.prieur@tetradis.com>
2023-11-23 16:05:36 +0100

    Read entire file ok

--- README.md
+++ README.md
@@ -10,6 +10,6 @@
 *  Créer un projet : `pipenv --python 3.11` -> créer Pipfile 
 *  Installer les dépendances du projet : `pipenv install` (ajouter des dépndances, comme "dev" par exemple : `--dev`) 
 *  Activer l'environnement virtuel : `pipenv shell` 
-*  Lancer des commandes à l'intérieur de l'env virtuel : `pipenv run` 
+*  Lancer des commandes à l'intérieur de l'env virtuel : `pipenv run <command>` 
 
 
--- bruteforce.py
+++ bruteforce.py
@@ -1,3 +1,28 @@
 
+import csv
 
-# donnes_par_action.txt
+
+# with open('eggs.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     for row in spamreader:
+#         print(', '.join(row))
+# Spam, Spam, Spam, Spam, Spam, Baked Beans
+# Spam, Lovely Spam, Wonderful Spam
+
+
+def read_the_infos(): 
+    """ Read the infos file to get the actions, their prices and their benefits. 
+        Returns:
+            list: the actions from the file. 
+    """ 
+    registered_actions = [] 
+    with open('data/donnees_par_action.csv', 'r') as file: 
+        reader = csv.reader(file, delimiter='\t') 
+        for row in reader: 
+            registered_actions.append(row) 
+    return registered_actions 
+
+
+registered_actions = read_the_infos() 
+print(registered_actions) 
+
